ccc31801ffbfdf60d5b9e64af1174e957b232222
kerz7 <50334310+kerz7@users.noreply.github.com>
2020-08-23 04:08:31 +0100

    Add files via upload

---
+++ esx_lockpick-master/config.lua
@@ -0,0 +1,37 @@
+Config						= {}
+Config.InfiniteLocks		= false  
+Config.LockTime			    = 30    
+Config.AlarmTime            = 60     
+Config.JammedHandbrakeTime  = 10     
+Config.IgnoreAbort			= true   
+Config.AllowMecano			= false
+Config.NPCVehiclesLocked    = true  
+Config.Locale				= 'en'   
+Config.percentage           = 100
+Config.CallCops             = false   
+Config.CallCopsPercent      = 0
+Config.chance               = 10     
+
+Config.blacklist = { -- vehicles that will always be locked when spawned naturally
+  "T20",
+  "police",
+  "police2",
+  "sheriff3",
+  "sheriff2",
+  "sheriff",
+  "riot",
+  "fbi",
+  "hwaycar2",
+  "hwaycar3",
+  "hwaycar10",
+  "hwaycar",
+  "polf430",
+  "policeb",
+  "police7",
+  "RHINO"
+}
+
+Config.job_whitelist = {
+"police",
+"EMS"
+}
